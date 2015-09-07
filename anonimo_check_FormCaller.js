e9ba13a073731c5f5fd3f801f63ae0118659bc92
velozmontero <velozmontero@gmail.com>
2015-09-07 01:07:27 -0400

    hello

--- anonimo_check_FormCaller.js
+++ anonimo_check_FormCaller.js
@@ -1,44 +1,56 @@
-function(){
+function() {
     function isValidName(name) {
-        var pattern = new RegExp(/^[a-z]{2,100}$/ig);
-        return pattern.test(name);
-    }
+            var pattern = new RegExp(/^[a-z]{2,100}$/ig);
+            return pattern.test(name);
+        }
 
-    function isValidEmail(address) {
-        var pattern = new RegExp(/^\w{2,100}@\w{2,100}.\w{1,4}$/ig);
-        return pattern.test(address);
-    }
+        function isValidEmail(address) {
+            var pattern = new RegExp(/^\w{2,100}@\w{2,100}.\w{1,4}$/ig);
+            return pattern.test(address);
+        }
 
-    function isValidPhone(phoneNum) {
-        var phone = new RegExp(/^[0-9]{3}[-. ]{0,1}[0-9]{3}[-. ]{0,1}[0-9]{4}$/);
-        return phone.test(phoneNum);
+        function isValidPhone(phoneNum) {
+            var phone = new RegExp(/^[0-9]{3}[-. ]{0,1}[0-9]{3}[-. ]{0,1}[0-9]{4}$/);
+            return phone.test(phoneNum);
+        }
+        /*
+                function validateFeedback(){
+                var phone = document.getElementById("phone");
+                var RE = /^[\d\.\-]+$/;
+                if(!RE.test(phone.value))
+                {
+                    alert("You have entered an invalid phone number");
+                    return false;
+                }
+                return true;
+            }
+                */
+        function validateForm(theForm) {
+            //event.preventDefault();
+            var fName = theForm.firstName.value;
+            var lName = theForm.lastName.value;
+            if (!isValidName(fName) || !isValidName(lName)) {
+                alert("Please enter a valid First and Last name");
+            }
+            var email = theForm.email.value;
+            if (!isValidEmail(email)) {
+                alert("Enter a valid email");
+                return false;
+            }
+            var phone = theForm.phoneNum.value;
+            if (!isValidPhone(phone)) {
+                alert("Enter a valid Phone Number");
+                return false;
+            }
+            console.log("validation successful");
+        }
+		
+    
+   
+    function callCheckForm() {
+        CheckForm1 (form);
     }
-    
-    
-    function CheckForm(theForm){
-        //event.preventDefault();
-        var fName = theForm.firstName.value;
-        var lName = theForm.lastName.value;
-        if (!isValidName(fName) || !isValidName(lName)) {
-            alert("Please enter a valid First and Last name");
-        }
-        var email = theForm.email.value;
-        if (!isValidEmail(email)) {
-            alert("Enter a valid email");
-            return false;
-        }
-        var phone = theForm.phoneNum.value;
-        if (!isValidPhone(phone)) {
-            alert("Enter a valid Phone Number");
-            return false;
-        }
-        console.log("validation successful");
-    }
-    
-    var form= document.getElementByID("elIDdelForm");
+     var form= document.getElementByID("elIDdelForm");
     form.addEventListener("submit", callCheckForm );
     
-    function callCheckForm(){
-        CheckForm1(form);
-    }
 }();
